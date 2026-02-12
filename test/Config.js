6698f710dfe71acb2007691d7ade5e1bdba5bbd2
9aoy <9aoyuao@gmail.com>
2026-02-12 15:13:16 +0800

    feat: support display function name when toString

--- test/Config.js
+++ test/Config.js
@@ -668,6 +668,22 @@ test('toString for functions with custom expression', () => {
   );
 });
 
+test('toString for long function ', () => {
+  const fn = function foo () { 
+    // This is a long function that exceeds the default maxLength for expressions in javascript-stringify
+  };
+
+  const config = new Config();
+
+  config.externals(fn);
+
+  expect(config.toString().trim()).toMatchInlineSnapshot(`
+    "{
+      externals: function foo() { /* omitted long function */ }
+    }"
+  `);
+});
+
 test('toString with custom prefix', () => {
   const config = new Config();
 
